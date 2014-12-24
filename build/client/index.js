a17e360f032bbbd74ae1676407301f1f5805cb8d
Joseph Silvestre <joseph.silvestre38@gmail.com>
2014-12-24 10:45:09 +0100

    Changed: build is now done during installation

--- build/client/index.js
+++
@@ -1,8 +0,0 @@
-var jade = require('jade/runtime');module.exports=
-function template(locals) {
-var buf = [];
-var jade_mixins = {};
-var jade_interp;
-;var locals_for_with = (locals || {});(function (imports) {
-buf.push("<!DOCTYPE html><html><head><title>Tasky</title><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\"><link rel=\"stylesheet\" href=\"styles/application.css?b8e2856\"></head><body><div class=\"main-container\"></div><script>" + (null == (jade_interp = imports) ? "" : jade_interp) + "</script><script src=\"scripts/bundle.min.js?b8e2856\"></script></body></html>");}.call(this,"imports" in locals_for_with?locals_for_with.imports:typeof imports!=="undefined"?imports:undefined));;return buf.join("");
-}
