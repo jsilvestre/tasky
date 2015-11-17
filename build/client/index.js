0adb5b3d1d2b1f09a33b2e4822c1f6fe136bbbb0
Joseph Silvestre <joseph.silvestre38@gmail.com>
2015-11-17 11:54:12 +0100

    Build

--- build/client/index.js
+++ build/client/index.js
@@ -4,5 +4,5 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 ;var locals_for_with = (locals || {});(function (imports) {
-buf.push("<!DOCTYPE html><html><head><title>Tasky</title><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\"><link rel=\"stylesheet\" href=\"styles/application.css?ad60f2e\"></head><body><div id=\"mount-point\"></div><script>" + (null == (jade_interp = imports) ? "" : jade_interp) + "</script><script src=\"scripts/bundle.js?ad60f2e\"></script></body></html>");}.call(this,"imports" in locals_for_with?locals_for_with.imports:typeof imports!=="undefined"?imports:undefined));;return buf.join("");
+buf.push("<!DOCTYPE html><html><head><title>Tasky</title><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\"><link rel=\"stylesheet\" href=\"styles/application.css?631f2aa\"></head><body><div id=\"mount-point\"></div><script>" + (null == (jade_interp = imports) ? "" : jade_interp) + "</script><script src=\"scripts/bundle.js?631f2aa\"></script></body></html>");}.call(this,"imports" in locals_for_with?locals_for_with.imports:typeof imports!=="undefined"?imports:undefined));;return buf.join("");
 }
