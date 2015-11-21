4f92140f242a92f8579ed9a4b92af40c5cde81a0
Joseph Silvestre <joseph.silvestre38@gmail.com>
2015-11-21 16:39:33 +0100

    Improve app initialization to make it less dependent on browser-specific things

---
+++ client/src/boot.js
@@ -0,0 +1,53 @@
+import Application from './components/application';
+import logger from 'debug';
+import React from 'react/addons';
+import { Provider } from 'react-redux';
+import startRouter from './router';
+import { configureStore } from './store.js';
+
+const debug = logger('app:boot');
+
+export default function boot(data) {
+    debug('Prepare application to boot.');
+
+    debug('Get localization strings based on given locale.');
+    const locale = data.locale;
+
+    const localesLoader = {
+        en: require('./locales/en'),
+        fr: require('./locales/fr'),
+    };
+
+    let phrases = localesLoader[locale];
+
+    if (!phrases) {
+        debug(`Localized strings could not be found for locale ${locale}, `
+              `using EN locale instead.`);
+        phrases = localesLoader.en;
+    }
+
+    // Initialize polyglot object with phrases.
+    const Polyglot = require('node-polyglot');
+    const polyglot = new Polyglot({locale: locale});
+    polyglot.extend(phrases);
+
+    const store = configureStore(data);
+    const history = startRouter(store);
+
+    const application = (
+        <Provider store={store}>
+            {() =>
+                <Application />
+            }
+        </Provider>
+    );
+
+    debug('Application configured, ready to start.');
+
+    return {
+        router: history,
+        store,
+        t: polyglot.t.bind(polyglot),
+        application,
+    };
+}
--- client/src/initialize.js
+++ client/src/initialize.js
@@ -1,61 +1,27 @@
-import Application from './components/application';
+import boot from './boot';
 import logger from 'debug';
 import React from 'react/addons';
-import { Provider } from 'react-redux';
-import startRouter from './router';
-import { configureStore } from './store.js';
 
 const debug = logger('app:init');
 
+// Executed when the browser has loaded everything.
 window.onload = () => {
     debug('`onload` event fired.');
-    debug('Initialize application.');
+    debug('Initialize and start application.');
 
-    debug('Get localization strings based on given locale.');
-    // TODO: make it independant of browser implementation.
-    const locale = window.locale;
-    delete window.locale;
+    // TODO: pass global functions to react components through context, not
+    // window.
+    const {t, router, application} = boot(window);
 
-    const localesLoader = {
-        en: require('./locales/en'),
-        fr: require('./locales/fr'),
-    };
-
-    let phrases = localesLoader[locale];
-
-    if (!phrases) {
-        debug(`Localized strings could not be found for locale ${locale}, `
-              `using EN locale instead.`);
-        phrases = localesLoader.en;
-    }
-
-    // Initialize polyglot object with phrases.
-    const Polyglot = require('node-polyglot');
-    const polyglot = new Polyglot({locale: locale});
-    polyglot.extend(phrases);
-
-    // Handy shortcut.
-    // TODO: make it independant of browser implementation.
-    window.t = polyglot.t.bind(polyglot);
-
-    debug('Initialize store.');
-    // TODO: make it independant of browser implementation.
-    const store = configureStore(window);
-
-    const history = startRouter(store);
+    // Handy localization shortcut.
+    window.t = t;
 
     // Helper to allow link creation all around the application.
-    // TODO: make it independant of browser implementation.
-    window.router = history;
+    window.router = router;
 
     debug('Mount React application');
     const node = document.querySelector('#mount-point');
-    React.render(
-        <Provider store={store}>
-            {() =>
-                <Application />
-            }
-        </Provider>, node);
+    React.render(application, node);
 
-    debug('Application initialized.');
+    debug('Application started.');
 };
